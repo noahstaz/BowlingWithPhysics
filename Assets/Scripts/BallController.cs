d8b4ad9ca4c0a2df348d17a07f8f7efdfbc0cb35
Noah Stasuik <97553023+noahstaz@users.noreply.github.com>
2025-02-02 12:27:50 -0800

    Prefabs created

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,7 +5,8 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
-
+    // name booleans like a question
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
     void Start()
@@ -19,9 +20,13 @@ public class BallController : MonoBehaviour
         inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
 
-
     private void LaunchBall()
     {  
+        // now your if check can be framed like a sentence
+        // "if ball is launched, then simply return and do nothing"
+        if (isBallLaunched) return;
+        // "now that the ball is not launched, set it to true and launch the ball"
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
