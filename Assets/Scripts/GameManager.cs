30367e3b86b869073f8a4fe279ba560f79e31c47
Noah Stasuik <97553023+noahstaz@users.noreply.github.com>
2025-02-07 14:19:18 -0800

    adding scoring system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
     private FallTrigger[] pins;
 
     private void Start()
@@ -17,5 +19,6 @@ public class GameManager : MonoBehaviour
     private void IncrementScore()
     {
         score++;
+        scoreText.text = $"Score: {score}";
     }
 }
